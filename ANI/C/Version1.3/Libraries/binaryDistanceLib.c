bb2fff67ac47b673085b24829753d9a0daf9d002
jbeacher6 <jon@jons-Mac-mini.local>
2017-08-21 18:53:05 -0700

    binary Distance algorithm

--- ANI/C/Version1.3/Libraries/binaryDistanceLib.c
+++ ANI/C/Version1.3/Libraries/binaryDistanceLib.c
@@ -3,16 +3,15 @@
 #include <stdlib.h>
 
 void binDistance(char* string1, char* string2) {
-    int ANI = 0;
+    int distance = 0;
     for(int i = 0; i < strlen(string1); i++) {
         if(i%2 == 0) {
             //printf("%d", i);
-            printf("\n%d", string1[i] - '0' ^ string2[i] - '0');
-            printf("%d\n", string1[i+1] - '0' ^ string2[i+1] - '0');
-            //ANI = ANI + (i ^ i2) + (i & i2);
-            ANI = ANI + ((string1[i] - '0' ^ string2[i] - '0') ^ (string1[i+1] - '0' ^ string2[i+1] - '0')) + ((string1[i] - '0' ^ string2[i] - '0') & (string1[i+1] - '0' ^ string2[i+1] - '0'));
+            //printf("\n%d", string1[i] - '0' ^ string2[i] - '0');
+            //printf("%d\n", string1[i+1] - '0' ^ string2[i+1] - '0');
+            distance = distance + ((string1[i] - '0' ^ string2[i] - '0') ^ (string1[i+1] - '0' ^ string2[i+1] - '0')) + ((string1[i] - '0' ^ string2[i] - '0') & (string1[i+1] - '0' ^ string2[i+1] - '0'));
         }
     }
     //printf("\nANI: %d\n", ANI);
-    return ANI;
+    return distance;
 }
